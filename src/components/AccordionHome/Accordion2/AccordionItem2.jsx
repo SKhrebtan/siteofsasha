b8583bf5c1454ca9f3f0c11a8f58b8eca489f8e4
SKhrebtan <shrebtan@gmail.com>
2023-11-23 15:45:53 +0200

    plus minus

--- src/components/AccordionHome/Accordion2/AccordionItem2.jsx
+++ src/components/AccordionHome/Accordion2/AccordionItem2.jsx
@@ -1,17 +1,26 @@
 import { useRef } from 'react'
-import {ReactComponent as ArrowSvg} from 'img/arrowdown2.svg'
+// import {ReactComponent as ArrowSvg} from 'img/arrowdown2.svg'
 import './Accordion2.css'
+import { ReactComponent as PlusSvg } from 'img/plus.svg';
+import { ReactComponent as MinusSvg } from 'img/minus.svg';
 
 export const AccordionItem2 = ({ faqItem, onClick, isOpen }) => {
     const itemRef = useRef(null)
     return (
             <li className={`accordion-item2 ${isOpen ? 'isopened' : ''}`}>
-            <button
+            {/* <button
                 onClick={()=>onClick()}
                 type='button'
                 className='accordion-header2'>
                 {faqItem.q}
                 <ArrowSvg className={`accordion-arrow ${isOpen ? 'active' : ''}`} />
+            </button> */}
+            <button
+                onClick={()=>onClick()}
+                type='button'
+                className='accordion-header2'>
+                {faqItem.q}
+                {isOpen ? <MinusSvg className='accordion-minus' /> : <PlusSvg className='accordion-plus' />}
             </button>
             <div
                 className="accordion-collapse"
